b27338ff2e63b3302b91bf9d02226226dc944fb6
Guranda Jikia <jikiaguranda@gmail.com>
2025-12-06 11:24:23 +0400

    completed task1

---
+++ task1.py
@@ -0,0 +1,16 @@
+# Define aPrint(in_list function that accepts ) #a list of strings and a separate string.
+# Return the index where the string exists in the list.
+# If the string does not exist, return -1.
+# Do NOT use the find or index methods.
+
+def in_list(lst, word):
+    for index, value in enumerate(lst):
+        if value == word:
+            return index
+    return -1 
+
+strings = ["enchanted", "sparks fly", "long live"]
+print(in_list(strings, "enchanted") ) # ==> 0
+print(in_list(strings, "sparks fly")) # ==> 1
+print(in_list(strings, "fifteen")   ) # ==> -1
+print(in_list(strings, "love story")) # ==> -1
---
+++ test.py
@@ -0,0 +1,5 @@
+
+strings = ["enchanted", "sparks fly", "long live"]
+for index, value in enumerate(strings):
+    print(index[value])
+    
