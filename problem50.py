7520581a04fd52b21a61fa06528afe51156be7cd
Guranda Jikia <jikiaguranda@gmail.com>
2025-12-07 17:52:32 +0400

    completed problem 50

---
+++ problem50.py
@@ -0,0 +1,15 @@
+# create set that display only even number
+numbers = set()
+def even_nums(numbers):
+    for i in range(5):
+        user_input = int(input("Enter a number :"))
+        numbers.add(user_input)
+    even_set = {num for num in numbers if num % 2 == 0}
+    return even_set
+   
+
+     
+     
+print(even_nums(numbers))
+    
+    
