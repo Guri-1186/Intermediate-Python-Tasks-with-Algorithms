30f10bacc7161b2a1b2c85de3bf4ca07407f8d0a
Guranda Jikia <jikiaguranda@gmail.com>
2025-12-08 22:15:57 +0400

    completes task 18

--- task18.py
+++ task18.py
@@ -1,8 +1,11 @@
-# Declare a destroy_elements function that accepts two lists.
+# Declare a estroy_elements function that accepts two lists.
 # It should return a list of all elements from the first list that are NOT contained in the second list.
 # Use list comprehension in your solution.
 #
 # EXAMPLES
-# destroy_elements([1, 2, 3], [1, 2])      => [3]
-# destroy_elements([1, 2, 3], [1, 2, 3])   => []
-# destroy_elements([1, 2, 3], [4, 5])      => [1, 2, 3]
+def destroy_elements(first, second):
+    return [element for element in first if element not in second]
+    
+print(destroy_elements([1, 2, 3], [1, 2])   ) #  => [3]
+print(destroy_elements([1, 2, 3], [1, 2, 3])) #  => []
+print(destroy_elements([1, 2, 3], [4, 5])   ) #  => [1, 2, 3]
