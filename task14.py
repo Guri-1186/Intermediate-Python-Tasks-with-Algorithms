c5a691401fcbe112b4ca806d25e92ca30f4ff731
Guranda Jikia <jikiaguranda@gmail.com>
2025-12-08 21:39:43 +0400

    completed task14

--- task14.py
+++ task14.py
@@ -1,8 +1,13 @@
 # Define a cleanup function that accepts a list of strings.
 # The function should return the strings joined together by a space.
 # There's one BIG problem -- some of the strings are empty or only consist of spaces!
-# These should NOT be included in the final string
-#
-# cleanup(["cat", "er", "pillar"])           => "cat er pillar"
-# cleanup(["cat", " ", "er", "", "pillar"])  => "cat er pillar"
-# cleanup(["", "", " "])                     => ""
+# These should NOT be included in 
+    
+    
+def cleanup(lst):
+  clean_list = [word for word in lst if word.strip() != ""]
+  return " ".join(clean_list)
+    
+print(cleanup(["cat", "er", "pillar"])         ) #  => "cat er pillar"
+print(cleanup(["cat", " ", "er", "", "pillar"])) #  => "cat er pillar"
+print(cleanup(["", "", " "])                   ) #  => ""
