10d93edf480b2ab2222806b027df654af2de683d
Guranda Jikia <jikiaguranda@gmail.com>
2025-12-08 21:52:23 +0400

    completed task 15

--- task15.py
+++ task15.py
@@ -1,8 +1,16 @@
-# Define a nested_sum function that accepts a list of lists of numbers
+# Define a ested_sum function that accepts a list of lists of numbers
 # The function should return the sum of the values
 # The list may contain empty lists
 #
 # EXAMPLES
-# nested_sum([[1, 2, 3], [4, 5]])            => 15
-# nested_sum([[1, 2, 3], [], [], [4], [5]])  => 15
-# nested_sum([[]])                           => 0
+def nested_sum(lst_of_lsts):
+    total = 0
+    for sublist in lst_of_lsts:
+        for num in sublist:
+            total += num
+    return total
+    
+print(nested_sum([[1, 2, 3], [4, 5]])          ) #  => 15
+print(nested_sum([[1, 2, 3], [], [], [4], [5]])) #  => 15
+print(nested_sum([[]])                         ) #  => 0
+
--- test.py
+++ test.py
@@ -1,3 +1,7 @@
-text = "abc"
-for i in text:
-    print(i)
+lists = [[1, 2, 3], [], [], [4], [5]]
+
+for nums in lists:
+    result = 0            # reset sum for each sublist
+    for char in nums:
+        result += char
+    print(result)
