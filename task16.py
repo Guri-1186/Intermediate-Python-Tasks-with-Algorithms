eb86a3c0ff4b1ba390875438e0b953e19e565cd1
Guranda Jikia <jikiaguranda@gmail.com>
2025-12-08 22:03:59 +0400

    completed task 16

--- task16.py
+++ task16.py
@@ -3,8 +3,18 @@
 # The strings in a list should only be concatenated if the length of the list is 3
 #
 # EXAMPLES
-# fancy_concatenate([["A", "B", "C"]])                        => "ABC"
-# fancy_concatenate([["A", "B", "C"], ["D", "E", "F"]])       => "ABCDEF"
-# fancy_concatenate([["A", "B", "C"], ["D", "E", "F", "G"]])  => "ABC"
-# fancy_concatenate([["A", "B", "C"], ["D", "E"]])            => "ABC"
-# fancy_concatenate([["A", "B"], ["C", "D"]])                 => ""
+
+def fancy_concatenate( lists):
+    final = ""
+    for row in lists:
+        if len(row) == 3:
+            for string in row:
+                final+=string
+    return final
+            
+        
+print(fancy_concatenate([["A", "B", "C"]])                      ) #  => "ABC"
+print(fancy_concatenate([["A", "B", "C"], ["D", "E", "F"]])     ) #  => "ABCDEF"
+print(fancy_concatenate([["A", "B", "C"], ["D", "E", "F", "G"]])) #  => "ABC"
+print(fancy_concatenate([["A", "B", "C"], ["D", "E"]])          ) #  => "ABC"
+print(fancy_concatenate([["A", "B"], ["C", "D"]])               ) #  => ""
--- test.py
+++ test.py
@@ -1,7 +1,2 @@
-lists = [[1, 2, 3], [], [], [4], [5]]
-
-for nums in lists:
-    result = 0            # reset sum for each sublist
-    for char in nums:
-        result += char
-    print(result)
+lists = [["A", "B", "C"], ["D", "E", "F"]]
+print(lists[0])
