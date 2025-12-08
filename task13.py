2e20c03c7f602cbe1754733ad07eac18c5994236
Guranda Jikia <jikiaguranda@gmail.com>
2025-12-08 21:23:05 +0400

    completed task 13

--- task13.py
+++ task13.py
@@ -2,5 +2,13 @@
 # It should return a list with the lengths of each word.
 #
 # EXAMPLES
-# word_lengths("Mary Poppins was a nanny")  => [4, 7, 3, 1, 5]
-# word_lengths("Somebody stole my donut")   => [8, 5, 2, 5]
+def word_lengths(sentence):
+    result = []
+    splited_word = sentence.split()
+    for word in splited_word:
+        result.append(len(word))
+    return result
+   
+    
+print(word_lengths("Mary Poppins was a nanny")) #  => [4, 7, 3, 1, 5]
+print(word_lengths("Somebody stole my donut") ) #  => [8, 5, 2, 5]
