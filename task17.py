b85f56aff424a36ea68bc58a5c20ab8d5fcb440b
Guranda Jikia <jikiaguranda@gmail.com>
2025-12-08 22:10:11 +0400

    completed task 17

--- task17.py
+++ task17.py
@@ -3,15 +3,18 @@
 # The floats variable should store the floating point values 
 # for each string in the values list.
 values = ["3.14", "9.99", "567.324", "5.678"]
-# floats = []
+floats = [float(el)for el in values]
+print(floats)
 
 # The letters variable should store a list of 5 strings. 
 # Each of the strings should be a character from name concatenated together 3 times.
 # i.e. ['BBB', 'ooo', 'rrr', 'iii', 'sss']
 name = "Boris"
-# letters = []
+letters = [char * 3 for char in name ]
+print(letters)
 
 # The 'lengths' list should store a list with the lengths
 # of each string in the 'elements' list
 elements = ["Hydrogen", "Helium", "Lithium", "Boron", "Carbon"]
-# lengths = []
+lengths = [len(word)for word in elements]
+print(lengths)
