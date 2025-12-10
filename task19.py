9ada2bef501396774807811f1f750314e32d9d28
Guranda Jikia <jikiaguranda@gmail.com>
2025-12-10 19:02:24 +0400

    completed task 19

--- task19.py
+++ task19.py
@@ -3,6 +3,11 @@
 # Do NOT use list comprehension.
 #
 # EXAMPLES:
-# right_words(['cat', 'dog', 'bean', 'ace', 'heart'], 3)     => ['cat', 'dog', 'ace']
-# right_words(['cat', 'dog', 'bean', 'ace', 'heart'], 5)     => ['heart']
-# right_words([], 4)                                         => []
+def right_words(words, num):
+    return list(filter(lambda word: len(word) == num, words))
+
+    
+   
+print(right_words(['cat', 'dog', 'bean', 'ace', 'heart'], 3) ) #    => ['cat', 'dog', 'ace']
+print(right_words(['cat', 'dog', 'bean', 'ace', 'heart'], 5) ) #    => ['heart']
+print(right_words([], 4)                                     ) #    => []
---
+++ task22.py
@@ -0,0 +1,10 @@
+# sum function
+
+# Declare a greater_sum function that accepts two lists of numbers.
+# It should return the list with the greatest sum.
+# You can assume the lists will always have different sums.
+#
+# EXAMPLES
+# greater_sum([1, 2, 3], [1, 2, 4]) => [1, 2, 4]
+# greater_sum([4, 5], [2, 3, 6])    => [2, 3, 6]
+# greater_sum([1], [])              => [1]
