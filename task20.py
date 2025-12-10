8731ba7010348bfe10373aba78e6d517a60c78e3
Guranda Jikia <jikiaguranda@gmail.com>
2025-12-10 19:06:16 +0400

    completed task 20

--- task20.py
+++ task20.py
@@ -4,5 +4,8 @@
 # Do NOT use list comprehension.
 #
 # EXAMPLES:
-# only_odds([1, 3, 5, 6, 7, 8])      =>  [1, 3, 5, 7]
-# only_odds([2, 4, 6, 8])            =>  []
+def only_odds(lst):
+    return list(filter(lambda num : num%2 == 1 ,lst))
+    
+print(only_odds([1, 3, 5, 6, 7, 8])) #     =>  [1, 3, 5, 7]
+print(only_odds([2, 4, 6, 8]))      #    =>  []
