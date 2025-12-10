c077597a8e0b3d09d274ed8e1a23272ac23073b9
Guranda Jikia <jikiaguranda@gmail.com>
2025-12-10 19:22:01 +0400

    completed task 22

--- task22.py
+++ task22.py
@@ -1,10 +1,17 @@
-# sum function
+
 
 # Declare a greater_sum function that accepts two lists of numbers.
 # It should return the list with the greatest sum.
 # You can assume the lists will always have different sums.
 #
 # EXAMPLES
-# greater_sum([1, 2, 3], [1, 2, 4]) => [1, 2, 4]
-# greater_sum([4, 5], [2, 3, 6])    => [2, 3, 6]
-# greater_sum([1], [])              => [1]
+def greater_sum(lst1,lst2):
+    if sum(lst1) > sum(lst2):
+        return lst1
+    else:
+        return lst2
+   
+      
+print( greater_sum([1, 2, 3], [1, 2, 4])) # => [1, 2, 4]
+print( greater_sum([4, 5], [2, 3, 6])   ) # => [2, 3, 6]
+print( greater_sum([1], [])             ) # => [1]
