762317768a92eac8745cc7ab4f3aac1eea8a8f4b
Guranda Jikia <jikiaguranda@gmail.com>
2025-12-10 19:11:03 +0400

    completed task 21

--- task21.py
+++ task21.py
@@ -1,8 +1,10 @@
-# Declare a count_of_a function that accepts a list of strings.
+# Declare count_of_a function that accepts a list of strings #
 # It should return a list with counts of how many “a” characters appear per string.
 # Do NOT use list comprehension.
 #
 # EXAMPLES:
-# count_of_a(["alligator", "aardvark", "albatross"])    => [2, 3, 2]
-# count_of_a(["plywood"])                               => [0]
-# count_of_a([])                                        => []
+def count_of_a(lst):
+    return list(map(lambda word: word.count("a"),lst))
+print(count_of_a(["alligator", "aardvark", "albatross"])) #   => [2, 3, 2]
+print(count_of_a(["plywood"])                           ) #   => [0]
+print(count_of_a([])                                    ) #   => []
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,10 @@
-
-            lst.pop(-1)
+# Declare count_of_a function that accepts a list of strings #
+# It should return a list with counts of how many “a” characters appear per string.
+# Do NOT use list comprehension.
+#
+# EXAMPLES:
+def count_of_a(lst):
+    return list(map(lambda word: word.count("a"),lst))
+print(count_of_a(["alligator", "aardvark", "albatross"])) #   => [2, 3, 2]
+print(count_of_a(["plywood"])                           ) #   => [0]
+print(count_of_a([])                                    ) #   => []
