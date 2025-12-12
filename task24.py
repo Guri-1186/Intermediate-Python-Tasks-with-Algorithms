93e1876e9f520815d8d65d4d0f3823511e90869d
Guranda Jikia <jikiaguranda@gmail.com>
2025-12-12 18:56:33 +0400

    completed task 24

---
+++ task24.py
@@ -0,0 +1,21 @@
+# Declare a months tuple with the last 4 months of the year (September, October, November, December) as strings.
+# Make sure the first letter of each month is capitalized.
+
+months = ("September", "October", "November", "December")
+
+# Create a faves variable with a list of your 3 three favorite movies as strings. 
+# Use the tuple function to convert the list to a tuple and save the result in a movies variable.
+faves = ["Anaconda", "Sweet November","You have got a Mail"]
+movies = tuple(faves)
+
+# Create a numbers_a, numbers_b, and numbers_c tuple. 
+# Each tuple should contain 3 integers. 
+# Declare an all_numbers tuple containing these three tuples.
+
+numbers_a = (2,3,4)
+
+numbers_b= (2,60,4)
+
+numbers_c = (2,30,4)
+
+all_numbers = (numbers_a,numbers_b,numbers_c)
