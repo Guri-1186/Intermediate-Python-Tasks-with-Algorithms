7ee7236a43cc55d95cefc3fc882be8b9840b1879
Guranda Jikia <jikiaguranda@gmail.com>
2025-12-16 20:43:01 +0400

    completed task 32

--- task32.py
+++ task32.py
@@ -1,4 +1,4 @@
-# Declare a sum_of_values function that accepts a dictionary and a list of strings.
+# Declare a sum_of_value function that accepts a dictionary and a list of strings.
 # The dictionary will have keys of strings and values of numbers.
 #
 # The function should return the sum of values for dictionary 
@@ -7,9 +7,17 @@
 # NOTE: sum is a reserved keyword in Python. Don’t use it as a variable name.
 #
 # EXAMPLES:
-# my_dict = { "a": 5, "b": 3, "c": 10 }
-#
-# sum_of_values(my_dict, ["a"])            => 5
-# sum_of_values(my_dict, ["a", "c"])       => 15
-# sum_of_values(my_dict, ["a", "c", "b"])  => 18
-# sum_of_values(my_dict, ["z"])            => 0
+my_dict = { "a": 5, "b": 3, "c": 10 }
+def sum_of_values(my_dict, lst):
+    result = 0
+    for key, value in my_dict.items():
+        if key in lst:
+            result+=value
+    return result
+
+  
+    
+print(sum_of_values(my_dict, ["a"]) )   #        => 5
+print(sum_of_values(my_dict, ["a", "c"]))   # => 15
+print(sum_of_values(my_dict, ["a", "c", "b"])) # => 18
+print(sum_of_values(my_dict, ["z"]))         #  => 0
